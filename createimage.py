bfdea68b7f567a483f2d0593766239b04e8ab6ac
940235carl <940235carl@gmail.com>
2021-01-23 15:03:50 +0800

    new

--- createimage.py
+++ createimage.py
@@ -3,6 +3,25 @@ import PIL.Image as Image
 import colorsys
 import numpy as np
 
+
+def MSE(np,op,bs):
+    imageSize1 = 768
+    imageSize2 = 512
+    totales = 0
+    for x in range(0,imageSize1):
+        for y in range(0,imageSize2):
+            r = np[x,y][0]-op[x,y][0]
+            g = np[x,y][1]-op[x,y][1]
+            b = np[x,y][2]-op[x,y][2]
+            #print(r,g,b)
+            totales = totales + (r*r+g*g+b*b)
+            
+    totales = totales/bs
+    mse = totales/(imageSize1*imageSize2)
+    
+    return mse
+    
+    
 if __name__ == "__main__":
     imageSize1 = 768
     imageSize2 = 512
@@ -48,11 +67,22 @@ if __name__ == "__main__":
                     image.putpixel((x,y),(pixel[x,y][0],PP,pixel[x,y][2]))
                 if z == 2 :
                     image.putpixel((x,y),(pixel[x,y][0],pixel[x,y][1],PP))
-        #print(pixel[x,y])      
-                
-                 
+     
+        
     image.save("resff.png")
     
+    
+    #以下為讀取信息，算取信息mse
+    
+    images = Image.open("resff.png").convert('RGB')
+    image = Image.open("kodim03.png").convert('RGB')
+    
+    pixels = images.load()
+    pix = image.load()
+     
+    ans = MSE(pixels,pix,3)
+    
+    print(ans)
     
     
     
